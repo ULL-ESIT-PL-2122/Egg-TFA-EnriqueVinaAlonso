6e7a7be6b7bcc5f129bdaf70538182aef6a90953
Enrique <alu0101337760@ull.edu.es>
2022-05-27 16:27:33 +0000

    Translation function returns the template

--- bin/egg.js
+++ bin/egg.js
@@ -2,8 +2,34 @@
 let fs = require('fs');
 const { program } = require('commander');
 const { version } = require('../package.json');
-let { runFromFile, translateToJs } = require('../lib/eggvm.js');
+let { runFromFile, compileToJS } = require('../lib/eggvm.js');
 let eggRepl = require('../lib/egg-repl');
+const jsBeautify = require('js-beautify');
+const jsBeautifyConfig = {
+  indent_size: 2,
+  space_in_empty_paren: true,
+  end_with_newline: true,
+  preserve_newlines: false
+}
+
+const compileToJsAndBeautify = (eggFile) => {  
+    let compiledJS = `
+    const path = require('path');
+    const runtimeSupport = require(
+      path.join(
+        '${__dirname}', 
+        "..", 
+        "generateJS", 
+        "runtimeSupport"
+      )
+    );
+    
+    ${compileToJS(eggFile)}
+    `;
+    
+    let compiledJSBW = jsBeautify(compiledJS, jsBeautifyConfig);
+    return compiledJSBW;
+  }
 
 program
     .version(version)
@@ -13,7 +39,7 @@ program
     .option("-j, --js <fileout>", "Path for output file. If it isn't specified the path of the origin file will be used,changing the extension to .js")
     .action((filename, options) => {
         if (filename && options.js) {
-           console.log(translateToJs(filename));
+           console.log(compileToJsAndBeautify(filename));
         } else if (filename) {
             runFromFile(filename);
         } else {
@@ -21,3 +47,5 @@ program
         }
     });
 program.parse(process.argv);
+
+
