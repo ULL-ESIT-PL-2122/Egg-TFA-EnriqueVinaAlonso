22e43d5a8fb3eaa5e093c287f4666f0a21a8a6f5
Enrique <alu0101337760@ull.edu.es>
2022-05-27 15:53:17 +0000

    Added generatedJSForms

--- bin/egg.js
+++ bin/egg.js
@@ -1,18 +1,20 @@
 #!/usr/bin/env node
 let fs = require('fs');
-const {program} = require('commander');
+const { program } = require('commander');
 const { version } = require('../package.json');
-let {runFromFile}  = require('../lib/eggvm.js');
+let { runFromFile, translateToJs } = require('../lib/eggvm.js');
 let eggRepl = require('../lib/egg-repl');
 
 program
     .version(version)
     .name('eggc')
     .description('Compile a Egg lang file')
-    .arguments("[filename]",  'The path of the file to compile')
-    .option("-o, --out <fileout>",  "Path for output file. If it isn't specified the path of the origin file will be used,changing the extension to .json")
+    .arguments("[filename]", 'The path of the file to compile')
+    .option("-j, --js <fileout>", "Path for output file. If it isn't specified the path of the origin file will be used,changing the extension to .js")
     .action((filename, options) => {
-        if (filename && options) {
+        if (filename && options.js) {
+           console.log(translateToJs(filename));
+        } else if (filename) {
             runFromFile(filename);
         } else {
             eggRepl();
