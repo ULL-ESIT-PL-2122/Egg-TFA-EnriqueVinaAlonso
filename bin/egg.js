067d7d6a589ae4110264a9a42ecf35c40003a492
Enrique <alu0101337760@ull.edu.es>
2022-05-28 20:31:40 +0000

    definitions work

--- bin/egg.js
+++ bin/egg.js
@@ -3,7 +3,6 @@ let fs = require('fs');
 const { program } = require('commander');
 const { version } = require('../package.json');
 let { runFromFile, compileToJS } = require('../lib/eggvm.js');
-let eggRepl = require('../lib/egg-repl');
 const jsBeautify = require('js-beautify');
 const jsBeautifyConfig = {
   indent_size: 2,
@@ -12,40 +11,44 @@ const jsBeautifyConfig = {
   preserve_newlines: false
 }
 
-const compileToJsAndBeautify = (eggFile) => {  
-    let compiledJS = `
+const compileToJsAndBeautify = (eggFile) => {
+  let compiledJS = `
     const path = require('path');
     const runtimeSupport = require(
       path.join(
         '${__dirname}', 
         "..", 
-        "generateJS", 
-        "runtimeSupport"
+        "lib", 
+        "runtime-support"
       )
     );
     
     ${compileToJS(eggFile)}
     `;
-    
-    let compiledJSBW = jsBeautify(compiledJS, jsBeautifyConfig);
-    return compiledJSBW;
-  }
+
+  let compiledJSBW = jsBeautify(compiledJS, jsBeautifyConfig);
+  return compiledJSBW;
+}
 
 program
-    .version(version)
-    .name('eggc')
-    .description('Compile a Egg lang file')
-    .arguments("[filename]", 'The path of the file to compile')
-    .option("-j, --js <fileout>", "Path for output file. If it isn't specified the path of the origin file will be used,changing the extension to .js")
-    .action((filename, options) => {
-        if (filename && options.js) {
-           console.log(compileToJsAndBeautify(filename));
-        } else if (filename) {
-            runFromFile(filename);
-        } else {
-            eggRepl();
-        }
-    });
+  .version(version)
+  .name('eggc')
+  .description('Compile a Egg lang file')
+  .arguments("[filename]", 'The path of the file to compile')
+  .option("-j, --js <fileout>", "Path for output file. If it isn't specified the path of the origin file will be used,changing the extension to .js")
+  .action((filename, options) => {
+    if (filename && options.js) {
+      let compiledJS = compileToJsAndBeautify(filename);
+      console.log(compiledJS);
+      console.log('\n\nResult = ' + eval(compiledJS));
+
+    } else if (filename) {
+      runFromFile(filename);
+    }
+    else {
+      console.log('No file specified');
+    }
+  });
 program.parse(process.argv);
 
 
