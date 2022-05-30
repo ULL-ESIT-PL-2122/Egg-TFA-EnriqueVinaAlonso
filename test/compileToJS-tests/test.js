5d65d920b91ee910956b723dd92833691a68c146
Enrique <alu0101337760@ull.edu.es>
2022-05-30 13:33:14 +0000

    Object seem to be done

--- test/compileToJS-tests/test.js
+++ test/compileToJS-tests/test.js
@@ -1,4 +1,5 @@
 
 const {compileToJsAndBeautify} = require("../../bin/egg.js");
+const {runFromFile} = require("../../lib/eggvm.js");
 
 console.log(compileToJsAndBeautify('test/compileToJS-tests/object.egg'));
